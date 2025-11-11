165c2b3984acc63b5d04d70120a6ff41251702c6
Andrew Wright <andrew98wright@gmail.com>
2025-11-11 15:12:12 -0500

    calculator app code added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 import sys
 
@@ -9,15 +10,21 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if len(sys.argv) < 2:
     print("Error: No propmpt provided.")
     sys.exit(1)
-prompt = sys.argv[1]
+user_prompt = sys.argv[1]
 
 client = genai.Client(api_key=api_key)
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents=prompt,
+    contents=messages,
 )
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if "--verbose" in sys.argv:
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
