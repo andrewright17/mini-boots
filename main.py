2678302fa71925f28a703fcbed4b279699108b84
Andrew Wright <andrew98wright@gmail.com>
2025-11-11 10:06:20 -0500

    prompt input from command line

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import os
 from google import genai
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+if len(sys.argv) < 2:
+    print("Error: No propmpt provided.")
+    sys.exit(1)
+prompt = sys.argv[1]
 
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    contents=prompt,
 )
 
 print(response.text)
