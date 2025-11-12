5c71f28aec2f346d401e6d0a524ef6421d2022cd
Andrew Wright <andrew98wright@gmail.com>
2025-11-11 21:56:15 -0500

    more tooling

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,6 @@
 import os
 from os.path import abspath
+from config import MAX_CHARS
 
 
 def get_files_info(working_directory, directory="."):
@@ -22,3 +23,29 @@ def get_files_info(working_directory, directory="."):
         f"- {content}: {size} bytes, is_dir={file_type}"
         for content, size, file_type in zip(contents, file_sizes, file_types)
     )
+
+
+def get_file_content(working_directory, file_path):
+    absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+    normalized_working_dir = os.path.abspath(working_directory)
+
+    if not absolute_path.startswith(normalized_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    if not os.path.isfile(absolute_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        # MAX_CHARS = 10000
+
+        with open(absolute_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        if len(file_content_string) == MAX_CHARS:
+            return (
+                file_content_string
+                + f"[...File {file_path} truncated at {MAX_CHARS} characters]"
+            )
+        return file_content_string
+
+    except Exception as e:
+        return f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,16 +1,14 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content
 import sys
 
-print("Results for current directory:")
-print(get_files_info("calculator", "."))
+# print(get_file_content("calculator", "lorem.txt"))
 
-print("Results for pkg/ directory:")
-print(get_files_info("calculator", "pkg"))
+print(get_file_content("calculator", "main.py"))
 
-print("Results for /bin directory:")
-print(get_files_info("calculator", "/bin"))
+print(get_file_content("calculator", "pkg/calculator.py"))
 
-print("Results for '../' directory:")
-print(get_files_info("calculator", "../"))
+print(get_file_content("calculator", "/bin/cat"))
+
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
 sys.exit(0)
